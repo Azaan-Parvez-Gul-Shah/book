ff2d2d6f18a0e415ed761685095632f49007e8ad
Azaan-Parvez-Gul-Shah <lazyalphamc@gmail.com>
2024-03-27 11:45:03 +0400

    book

---
+++ script.js
@@ -0,0 +1,9 @@
+// You can add JavaScript functionality here if needed
+// Add an event listener to the button
+document.getElementById('openPdfButton').addEventListener('click', function() {
+    // Replace 'path/to/your/file.pdf' with the actual path to your PDF file
+    var pdfPath = 'C:\Users\dell\Downloads\Azaan_Through_The_Times_.pdf';
+
+    // Open the PDF in a new window
+    window.open(pdfPath, 'C:\Users\dell\Downloads\Azaan_Through_The_Times_.pdf');
+});
